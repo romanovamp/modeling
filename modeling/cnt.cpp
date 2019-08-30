0c34f3a09dab8a3f7f687983eb5984b3d2da18fb
romanovamp <romanovamp@yandex.ru>
2019-08-31 00:48:07 +0500

    добавлен класс cnt

--- modeling/cnt.cpp
+++ modeling/cnt.cpp
@@ -1,11 +1,24 @@
 #include "stdafx.h"
 #include "cnt.h"
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
 
+namespace bg = boost::geometry;
+typedef bg::model::d2::point_xy<double> point_type;
+typedef bg::model::polygon<point_type> polygon_type;
 
 cnt::cnt()
 {
 }
 
+cnt::cnt(double _k, double _a, point_type _stratPoint, polygon_type _poly)
+{
+	k = _k;
+	a = _a;
+	stratPoint = _stratPoint;
+	poly = _poly;
+}
 
 cnt::~cnt()
 {
--- modeling/cnt.h
+++ modeling/cnt.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+typedef boost::geometry::model::d2::point_xy<double> point_type;
+typedef boost::geometry::model::polygon<point_type> polygon_type;
+
 class cnt
 {
 public:
+	double a, k;
+	point_type stratPoint;
+	polygon_type poly;
 	cnt();
+	cnt(double k, double a, point_type stratPoint, polygon_type poly);
 	~cnt();
 };
 
